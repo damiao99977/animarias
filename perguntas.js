89b2b9e98d68c913de2f85b54d45c83eb941d01c
neyzin.hy ( •̀ ω •́ )✧Damian <00001124897793sp@al.educacao.sp.gov.br>
2024-10-22 11:26:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Yaoi',
+    'Recomendação +16',
+    'Umibe no stranger'
+)
+
+criaCartao(
+    'Shounen',
+    'Recomendação +13',
+    'macha-Ude'
+)
+
+criaCartao(
+    'shoujo',
+    'Recomendação Livre para todas idades',
+    'Kimi ni todoke'
+)
+
+criaCartao(
+    'minecraft',
+    'Recomendação +Семнадцать',
+    'Haw cekpet'
+)
